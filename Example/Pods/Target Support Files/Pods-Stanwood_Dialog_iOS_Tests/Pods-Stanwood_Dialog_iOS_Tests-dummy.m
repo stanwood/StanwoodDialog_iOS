8cbe5257bcdd2a5aee46dfd94b908148a6ceefbc
Eugene Peschard <e@peschard.me>
2017-11-23 22:01:13 +0100

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-Stanwood_Dialog_iOS_Tests/Pods-Stanwood_Dialog_iOS_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Stanwood_Dialog_iOS_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Stanwood_Dialog_iOS_Tests
+@end
---
+++ Example/Pods/Target Support Files/Stanwood_Dialog_iOS/Stanwood_Dialog_iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Stanwood_Dialog_iOS : NSObject
+@end
+@implementation PodsDummy_Stanwood_Dialog_iOS
+@end
